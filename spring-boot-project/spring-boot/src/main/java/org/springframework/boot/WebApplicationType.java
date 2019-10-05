24334772e63cc53a016706bcb3e85c099f3120dd
liushipeng <liushipeng@aliyun.com>
2019-10-05 18:04:32 +0800

    1.检测应用的类型

--- spring-boot-project/spring-boot/src/main/java/org/springframework/boot/WebApplicationType.java
+++ spring-boot-project/spring-boot/src/main/java/org/springframework/boot/WebApplicationType.java
@@ -58,6 +58,10 @@ public enum WebApplicationType {
 
 	private static final String REACTIVE_APPLICATION_CONTEXT_CLASS = "org.springframework.boot.web.reactive.context.ReactiveWebApplicationContext";
 
+	/**
+	 *    检测  应用的类型
+	 * @return
+	 */
 	static WebApplicationType deduceFromClasspath() {
 		if (ClassUtils.isPresent(WEBFLUX_INDICATOR_CLASS, null) && !ClassUtils.isPresent(WEBMVC_INDICATOR_CLASS, null)
 				&& !ClassUtils.isPresent(JERSEY_INDICATOR_CLASS, null)) {
